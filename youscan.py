85fc1c3169db9f04cb015aa00c2d3794c4961c2b
juancuellarsol <166358338+juancuellarsol@users.noreply.github.com>
2025-09-25 16:09:21 -0500

    Fix indentation for date_series assignment

--- youscan.py
+++ youscan.py
@@ -122,7 +122,7 @@ def process_dataframe(
     #    ts.dt.day.astype("Int64").astype(str) + "/" +
     #    ts.dt.year.astype("Int64").astype(str)
     #)
-   date_series = pd.to_datetime(out[col])
+    date_series = pd.to_datetime(out[col])
 
 
 # Parseamos HH:MM (24h) y lo pasamos a 12h con segundos en :00
