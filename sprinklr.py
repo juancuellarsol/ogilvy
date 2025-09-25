2ae1321b8dc305e4dd972e3d5e1a1a829b137217
juancuellarsol <166358338+juancuellarsol@users.noreply.github.com>
2025-09-25 16:02:09 -0500

    Refactor date handling in sprinklr.py
    Removed commented-out code for date formatting and simplified date parsing.

--- sprinklr.py
+++ sprinklr.py
@@ -112,12 +112,7 @@ def process_dataframe(
     out[col] = _ensure_naive(out[col])
 
     # Fecha y hora (12h AM/PM sin cero inicial)
-    #date_series = out[col].dt.day.astype("Int64").astype(str) + "/" + \
-    #              out[col].dt.month.astype("Int64").astype(str) + "/" + \
-    #              out[col].dt.year.astype("Int64").astype(str)
-    #date_series = pd.to_datetime(date_series, format="%m/%d/%Y", errors="coerce")
-    #date_series = out[col].dt.normalize() 
-    #date_series = out[col].dt.strftime("%d/%m/%Y")
+
     date_series = pd.to_datetime(out[col])
     
     hora_series = out[col].dt.strftime("%I:%M:%S %p").str.lstrip("0")
